2f751363b7c93cc34382f62e4cf569bbf867cd53
João Szlachta <jpbs2002@gmail.com>
2024-09-29 21:52:57 -0300

    chore: create files and first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const initalDiv = document.getElementById("initial");
+//     const informationsDiv = document.getElementById("informations");
+//     const button = document.getElementById("btn");
+
+//     button.addEventListener("click", () => {
+//         initalDiv.style.display = "none";
+//         informationsDiv.style.display = "flex";
+//     })
+// });
+
